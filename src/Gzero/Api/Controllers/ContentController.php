ff1c6b13a7d666c7b05f78476e6fd55ee53da0be
Adrian Skierniewski <adrian.skierniewski@gmail.com>
2014-03-04 10:12:50 +0100

    Composer dev dependency

--- src/Gzero/Api/Controllers/ContentController.php
+++ src/Gzero/Api/Controllers/ContentController.php
@@ -24,14 +24,82 @@ class ContentController extends Controller {
         $this->contentRepo = $content;
     }
 
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
     public function index()
     {
         $list = $this->contentRepo->listBy()->get();
         return $this->contentRepo->loadTranslations($list);
     }
 
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @return Response
+     */
+    public function store()
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int $id
+     *
+     * @return Response
+     */
     public function show($id)
     {
         return $this->contentRepo->getById($id);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int $id
+     *
+     * @return Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  int $id
+     *
+     * @return Response
+     */
+    public function update($id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int $id
+     *
+     * @return Response
+     */
+    public function destroy($id)
+    {
+        //
     }
 } 
