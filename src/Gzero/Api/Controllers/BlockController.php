98c58196441a5ad2563872b74461f2649e968a1f
Adrian Skierniewski <adrian.skierniewski@gmail.com>
2014-03-08 11:50:00 +0100

    BlockController

---
+++ src/Gzero/Api/Controllers/BlockController.php
@@ -0,0 +1,104 @@
+<?php namespace Gzero\Api\Controllers;
+
+use Gzero\Repositories\Block\BlockRepository;
+use Illuminate\Routing\Controller;
+
+/**
+ * This file is part of the GZERO CMS package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * Class BlockController
+ *
+ * @package    Gzero\Admin\Controllers\Resource
+ * @author     Adrian Skierniewski <adrian.skierniewski@gmail.com>
+ * @copyright  Copyright (c) 2014, Adrian Skierniewski
+ */
+class BlockController extends Controller {
+
+    protected $blockRepo;
+
+    public function __construct(BlockRepository $block)
+    {
+        $this->blockRepo = $block;
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index()
+    {
+        return $this->blockRepo->listBy()->get();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @return Response
+     */
+    public function store()
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int $id
+     *
+     * @return Response
+     */
+    public function show($id)
+    {
+        return $this->blockRepo->getById($id);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int $id
+     *
+     * @return Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  int $id
+     *
+     * @return Response
+     */
+    public function update($id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int $id
+     *
+     * @return Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+} 
--- src/routes.php
+++ src/routes.php
@@ -3,6 +3,7 @@
 Route::group(
     array('prefix' => 'api/v1', 'before' => 'auth|admin'),
     function () {
+        Route::resource('block', 'Gzero\Api\Controllers\BlockController');
         Route::resource('content', 'Gzero\Api\Controllers\ContentController');
     }
 );
